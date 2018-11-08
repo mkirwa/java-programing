cb283f04174786f10ad8fb6cd8831c518c8b839f
mkirwa <mahlonkibiwott@gmail.com>
2018-11-08 15:53:33 -0600

    Add files via upload

---
+++ LibraryCatalogue.java
@@ -0,0 +1,29 @@
+//hackerrank day 11 tutorial video
+//what's in a catalogue?
+//1. What's available 
+//2. Check in a book
+//3. Check out a book
+//4. Container for books
+//5. Is a given book returned late?
+//6. Is book checked out?
+//we will be using multiple classes for this 
+
+//Library catalogue class
+//1. How long you can check out 
+//2. What's in this library
+//3. Plus more
+
+//Book class
+//1. Title
+//2. ISBN
+//3. Page count
+//4, What defines a book
+
+
+
+public class LibraryCatalogue{
+
+	public static void main(String[]args){
+
+	}
+}
