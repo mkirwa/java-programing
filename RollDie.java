186dca6199f4137829e278bba59a3d7d51b6206f
mkirwa <mahlonkibiwott@gmail.com>
2018-11-09 14:27:28 -0600

    Add files via upload

--- RollDie.java
+++ RollDie.java
@@ -7,16 +7,18 @@ public class RollDie{
 		Random randomNumbers = new Random();
 		int frequency[] = new int [7];
 		//roll die 6,000,000 times; use die value as frequency
-		for(int roll = 1; roll<=6000000; roll++)
+		for(int roll = 1; roll<=6000000; roll++){
 
 			++frequency[1+randomNumbers.nextInt(6)];
+		}
 
 		System.out.printf("%s%10s\n","Face","frequency");
 
 			//output each array element's value 
 
-		for(int face = 1; face<frequency.length; face++)
+		for(int face = 1; face<frequency.length; face++){
 				System.out.printf("%4d%10d\n",face,frequency[face]);
+		}
 			
 	}
 
