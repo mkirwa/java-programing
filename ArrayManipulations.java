b12f5eb34163a96ada4ce895a45feba2ed37c3d3
mkirwa <mahlonkibiwott@gmail.com>
2018-11-15 12:13:27 -0600

    Add files via upload

---
+++ ArrayManipulations.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class ArrayManipulations
+{
+	public static void main(String[] args){
+
+		double[] doubleArray = {8.4,9.3,0.2,7.9,3.4};
+		Arrays.sort(doubleArray);
+		//sorting an array
+		for(double value: doubleArray)
+			System.out.printf("%.1f",value);
+		//fill 10-element array with 7s
+		int[] filledIntArray = new int[10];
+		//fills filledIntArray with 7s
+		Arrays.fill(filledIntArray,7);
+		displayArray(filledIntArray);
+
+		//copy array intArray into array intArrayCopy
+		int intArray = {1,2,3,4,5,6};
+		int[] intArrayCopy = new int[ intArray.length ];
+		System.arraycopy( intArray, 0, intArrayCopy, 0, intArray.length );
+		displayArray( intArray, "intArray" );
+		displayArray( intArrayCopy, "intArrayCopy" );
+
+		// compare intArray and intArrayCopy for equality
+		boolean b = Arrays.equals( intArray, intArrayCopy );
+		System.out.printf( "\n\nintArray %s intArrayCopy\n",
+			( b ? "==" : "!=" ) );
+
+		// compare intArray and filledIntArray for equality
+		b = Arrays.equals( intArray, filledIntArray );
+		System.out.printf( "intArray %s filledIntArray\n",
+			( b ? "==" : "!=" ) );
+
+		//search intArray for the value 5
+		int location = Arrays.binarySearch(intArray, 5 );
+
+		if(location>=0)
+			System.out.printf("Found 5 at element %d in intArray\n", location );
+		else
+			System.out.println( "5 not found in intArray" );
+
+		// search intArray for the value 8763
+		location = Arrays.binarySearch( intArray, 8763 );
+		if(location>=0)
+			System.out.printf("Found 8763 at element %d in intArray\n", location );
+		else
+			System.out.println( "8763 not found in intArray" );
+
+	}
+
+   public static void displayArray( int[] array, String description )
+   {
+
+   		System.out.printf( "\n%s: ", description );
+
+   		for ( int value : array )
+   			System.out.printf( "%d ", value );
+
+
+   }
+   
+
+}
