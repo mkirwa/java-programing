ea3a2034f12ed0ee9a66812866b6283daf26a1c3
mkirwa <mahlonkibiwott@gmail.com>
2018-11-15 10:15:14 -0600

    Add files via upload

--- VarargsTest.java
+++ VarargsTest.java
@@ -14,4 +14,14 @@ public class VarargsTest{
 			total+=d;
 		return total/numbers.length;
 	}
+
+	public static void main(String[]args){
+
+		double  d1 = 10;
+		double d2 = 20;
+		double d3 = 30;
+		double d4 = 40;
+
+		System.out.printf("the average of d1%d and d2%d is: %d2\n",d1,d1,average(d1,d2))
+	}
 }
